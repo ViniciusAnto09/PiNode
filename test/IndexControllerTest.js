a655e4406fb7c2dd1acc69ba38bc6ffbb0cee13c
Vinicius Antonio <viniciusanto09@gmail.com>
2015-06-19 09:27:16 -0300

    adicionando teste no projeto

--- test/IndexControllerTest.js
+++ test/IndexControllerTest.js
@@ -3,7 +3,10 @@ var expect = require("expect");
 
 describe("IndexController", function() {
 	it("Espero quando passar 2 e 3 retorne 5", function() {
-		//var retorno = IndexController.somar2Num(2,3);
+		var retorno = IndexController.somar2Num(2,3);
+		var esperado = 5;
+		
+		expect(retorno).toEqual(esperado);
 		
 		
 	});
