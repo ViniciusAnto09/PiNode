40272ca09f16d3c35f0203bbb83651690a483282
Vinicius Antonio <viniciusanto09@gmail.com>
2015-06-23 23:27:40 -0300

    Add os teste de somar,subtrair e multiplicar

--- test/IndexControllerTest.js
+++ test/IndexControllerTest.js
@@ -7,17 +7,20 @@ describe("IndexController", function() {
 		var esperado = 5;
 		
 		expect(retorno).toEqual(esperado);
-		
-		
 	});
 	
 	it("Espero quando passar 0 e 3 retorne 3", function() {
 		var retorno = IndexController.somar2Num(0,3);
 		var esperado = 3;
 		
-		expect(retorno).toEqual(esperado);
+		expect(retorno).toEqual(esperado);	
+	});
+	
+	it("Espero quando passar 3 e -2 retorne 1", function() {
+		var retorno = IndexController.somar2Num(3,-2);
+		var esperado = 1;
 		
-		
+		expect(retorno).toEqual(esperado);	
 	});
 	
 	it("Espero quando passar 3 e 2 retorne 1", function() {
@@ -25,32 +28,41 @@ describe("IndexController", function() {
 		var esperado = 1;
 		
 		expect(retorno).toEqual(esperado);
+	});
+	
+	it("Espero quando passar 3 e 0 retorne 3", function() {
+		var retorno = IndexController.subtrair2Num(3,0);
+		var esperado = 3;
 		
-	})
+		expect(retorno).toEqual(esperado);
+	});
 	
 	it("Espero quando passar 2 e 3 retorne -1", function() {
 		var retorno = IndexController.subtrair2Num(2,3);
 		var esperado = -1;
 		
 		expect(retorno).toEqual(esperado);
-		
-	})
+	});
 	
 	it("Espero quando passar 2 e 3 retorne 6", function() {
 		var retorno = IndexController.multiplicar2Num(2,3);
 		var esperado = 6;
 		
-		expect(retorno).toEqual(esperado);
-		
-		
-	})
+		expect(retorno).toEqual(esperado);	
+	});
 	
 	it("Espero quando passar 2 e 0 retorne 0", function() {
 		var retorno = IndexController.multiplicar2Num(2,0);
 		var esperado = 0;
 		
 		expect(retorno).toEqual(esperado);
+	});
+	
+	it("Espero quando passar -1 e -1 retorne 1", function() {
+		var retorno = IndexController.multiplicar2Num(-1,-1);
+		var esperado = 1;
 		
-		
-	})
+		expect(retorno).toEqual(esperado);
+	});
+	
 });
